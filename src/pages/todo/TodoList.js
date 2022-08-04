f384a20a265dac841744fcb272e7608171fb3969
petahseo <94450217+petahseo@users.noreply.github.com>
2022-08-04 22:59:31 +0900

    TodoList

--- src/pages/todo/TodoList.js
+++ src/pages/todo/TodoList.js
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 export default function TodoList() {
+  const [todos, setTodos] = useState("");
+
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "todo") {
+      setTodos(value);
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
       <h1>Todo List</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="todo"
+          placeholder="todo"
+          value={todos}
+          required
+          onChange={onChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
