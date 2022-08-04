f42309529d08a3a54e14f0d291f9d42ea0c5b5e9
petahseo <94450217+petahseo@users.noreply.github.com>
2022-08-05 02:28:07 +0900

    styled component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./Router";
+import GlobalStyle from "./styles/GlobalStyles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
+    <GlobalStyle />
     <App />
   </>
 );
