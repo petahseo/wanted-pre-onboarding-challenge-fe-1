1cd87a77ea709aeb2dc4dbde8e841bb66e796220
petahseo <94450217+petahseo@users.noreply.github.com>
2022-08-12 00:22:38 +0900

    TodoList

--- src/pages/todo/TodoList.js
+++ src/pages/todo/TodoList.js
@@ -2,47 +2,20 @@ import styled from "styled-components";
 
 export default function TodoList() {
   return (
-    <Container>
-      <InputForm>
-        <InputTitle
-          type="text"
-          name="title"
-          placeholder="제목"
-          //value={todos.title}
-          required
-          //onChange={onChange}
-        />
-        <InputContent
-          type="text"
-          name="content"
-          placeholder="내용"
-          //value={todos.content}
-          required
-          //onChange={onChange}
-        />
-      </InputForm>
-    </Container>
+    <TodoForm>
+      <Todos>제목</Todos>
+      <Todos>내용</Todos>
+    </TodoForm>
   );
 }
 
-const Container = styled.div``;
-const InputForm = styled.div``;
-const InputTitle = styled.input`
+const TodoForm = styled.ul`
   position: absolute;
-  padding: 10px;
-  width: 240px;
-  height: 40px;
-  left: 23px;
-  top: 470px;
-  border: 1px solid #c4c4c4;
+  padding: 20px;
+  width: 329px;
+  height: 68px;
+  left: 20px;
+  top: 450px;
 `;
 
-const InputContent = styled.input`
-  position: absolute;
-  padding: 10px;
-  width: 240px;
-  height: 40px;
-  left: 23px;
-  top: 514px;
-  border: 1px solid #c4c4c4;
-`;
+const Todos = styled.li``;
